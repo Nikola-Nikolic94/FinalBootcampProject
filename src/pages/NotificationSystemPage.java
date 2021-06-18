dda81527b4ca98440bad7691d4fb0fa20bac81b5
Nikola Nikolic <Nikolic.Nikola94@outlook.com>
2021-06-18 19:08:12 +0200

    Worked on NotificationSystemPage class

--- src/pages/NotificationSystemPage.java
+++ src/pages/NotificationSystemPage.java
@@ -1,12 +1,24 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NotificationSystemPage extends BasicPage{
 
 	public NotificationSystemPage(WebDriver driver) {
 		super(driver);
 	}
-
-	
+	public WebElement getAlertMsg() {
+		return driver.findElement(By.xpath("//*[contains(@class,'system_message')]"));
+	}
+	public String getAlertMsgText() {
+		return getAlertMsg().getText();
+	}
+	public void waitUntilMsgDisappears() {
+		WebDriverWait wait = new WebDriverWait(driver,30);
+		wait.until(ExpectedConditions.attributeContains(getAlertMsg(), "style", "display:none"));
+	}
 }
