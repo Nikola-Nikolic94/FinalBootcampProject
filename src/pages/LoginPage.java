766b45ffe2f91ff540b6cb5c29d60a41fe3192cf
Nikola Nikolic <Nikolic.Nikola94@outlook.com>
2021-06-18 18:45:47 +0200

    Working on LoginPAge class

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,10 +1,28 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage extends BasicPage{
 
 	public LoginPage(WebDriver driver) {
 		super(driver);
+	}
+	public WebElement getUsername() {
+		return driver.findElement(By.name("username"));
+	}
+	public WebElement getPassword() {
+		return driver.findElement(By.name("password"));
+	}
+	public WebElement getSubmit() {
+		return driver.findElement(By.name("btn_submit"));
+	}
+	public void SignIn(String email, String password) {
+		getUsername().clear();
+		getPassword().clear();
+		getUsername().sendKeys(email);
+		getPassword().sendKeys(password);
+		getSubmit().click();
 	}
 }
