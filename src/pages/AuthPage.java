3fe944cca6b5ac9f1768e4211634f4e0551944c2
Nikola Nikolic <Nikolic.Nikola94@outlook.com>
2021-06-23 01:03:20 +0200

    Worked on AuthPage class

--- src/pages/AuthPage.java
+++ src/pages/AuthPage.java
@@ -1,11 +1,22 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class AuthPage extends BasicPage{
 
 	public AuthPage(WebDriver driver) {
 		super(driver);
 	}
-
+	public WebElement getLogoutDropDown() {
+		return driver.findElement(By.className("accounts-link"));
+	}
+	public WebElement getLogoutBtn() {
+		return driver.findElement(By.xpath("//div[@class='my-account-dropdown']/ul/li[2]"));
+	}
+	public void Logout() {
+		getLogoutDropDown().click();
+		getLogoutBtn().click();
+	}
 }
