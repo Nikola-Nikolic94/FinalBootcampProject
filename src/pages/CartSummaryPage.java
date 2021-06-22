eb45cff52607a1408380161cc5ae33eaab2c6340
Nikola Nikolic <Nikolic.Nikola94@outlook.com>
2021-06-23 01:04:18 +0200

    Worked on CartSummaryPage class

--- src/pages/CartSummaryPage.java
+++ src/pages/CartSummaryPage.java
@@ -1,11 +1,18 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class CartSummaryPage extends BasicPage{
 
 	public CartSummaryPage(WebDriver driver) {
 		super(driver);
 	}
-
+	public WebElement getClearAllBtn() {
+		return driver.findElement(By.xpath("//a[normalize-space()='Clear All']"));
+	}
+	public void clearAllFromCart() {
+		getClearAllBtn().click();
+	}
 }
