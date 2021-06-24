85a791dbfbb8b66b53b1e00b1c65f3b483e026d9
Nikola Nikolic <Nikolic.Nikola94@outlook.com>
2021-06-24 17:18:53 +0200

    Added screenshot function in the AfterMethod in BasicTest class

--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -2,18 +2,24 @@ package tests;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
+
+import com.google.common.io.Files;
+import com.sun.net.httpserver.Authenticator.Result;
 
 import pages.AuthPage;
 import pages.CartSummaryPage;
@@ -30,6 +36,7 @@ public abstract class BasicTest {
 	protected WebDriver driver;
 	protected WebDriverWait wait;
 	protected JavascriptExecutor js;
+	protected SoftAssert softAssert;
 	protected File file;
 	protected FileInputStream fis;
 	protected XSSFWorkbook workbook;
@@ -56,6 +63,7 @@ public abstract class BasicTest {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+		softAssert = new SoftAssert();
 		this.file = new File("./data/FirstDataForm.xlsx");
 		this.fis = new FileInputStream(file);
 		this.workbook = new XSSFWorkbook(fis);
@@ -70,7 +78,17 @@ public abstract class BasicTest {
 	}
 	
 	@AfterMethod
-	public void cleanup() {
-		//driver.close();
+	public void cleanup(ITestResult result) throws IOException {
+		String location = "./Screenshots/";
+		String methodName = result.getName();
+		if(ITestResult.FAILURE==result.getStatus()) {
+			try {
+				File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+				Files.copy(srcFile, new File(location + methodName + "_" + ".png"));
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}	
+			driver.close();
 	}
 }
